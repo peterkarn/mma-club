517280405a76bb8eab68f67430da8219fe6071fa
Peter <peterkarnaukh@gmail.com>
2020-12-04 10:57:55 +0200

    added background anr form fields was reordered

--- #src/js/script.js
+++ #src/js/script.js
@@ -2,19 +2,22 @@
 
 let navToggle = document.querySelector(".nav__toggle");
 let navWrapper = document.querySelector(".nav__wrapper");
+let pageHeader = document.querySelector(".site-header");
 
 navToggle.addEventListener("click", function () {
 	if (navWrapper.classList.contains("active")) {
 		this.setAttribute("aria-expanded", "false");
 		this.setAttribute("aria-label", "menu");
 		navWrapper.classList.remove("active");
-		document.querySelector('.nav-icon1').classList.remove('open');
+    document.querySelector('.nav-icon1').classList.remove('open');
+    pageHeader.classList.remove('scrollable');
 	} else {
 		navWrapper.classList.add("active");
 		this.setAttribute("aria-label", "close menu");
 		this.setAttribute("aria-expanded", "true");
-		document.querySelector('.nav-icon1').classList.add('open');
-		navWrapper.style.paddingBottom = height + "px";
+    document.querySelector('.nav-icon1').classList.add('open');
+    pageHeader.classList.add('scrollable');
+    
 	}
 });
 
@@ -50,25 +53,24 @@ const swiper = new Swiper('.news__slider', {
 });
 
 const swiper2 = new Swiper('.gallery__container', {
-    slidesPerView: 5,
-    // spaceBetween: 15,
-    slidesPerColumn: 2,
-    slidesPerColumnFill: 'row',
-    slidesPerGroup: 10,
     pagination: {
       el: '.swiper-pagination',
       clickable: true,
   },
-    //  breakpoints: {
-    //        1200: {
-    //           slidesPerView: 'auto',
-    //           spaceBetween: 40,
-    //        },
-    //        830: {
-    //           slidesPerView: 'auto',
-    //           spaceBetween: 10, //
-    //        }
-    //     }
+  breakpoints: {
+        320: {
+      slidesPerView: 1,
+          slidesPerGroup: 1,
+          slidesPerColumn: 1,
+          centeredSlides: true,
+    },
+      768: {
+        slidesPerView: 5,
+        slidesPerColumn: 2,
+        slidesPerColumnFill: 'row',
+        slidesPerGroup: 10,
+      }
+    }
 });
 
 const addFieldButton = document.querySelector('.form__add'),
@@ -120,3 +122,4 @@ const checkbox = document.querySelector('.form__policy input[type="checkbox"]');
 checkbox.addEventListener('change', function () {
   document.querySelector('.form__btn').removeAttribute("disabled");
 });
+
